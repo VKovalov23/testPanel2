76999d88140b443d412cc60b0530e3b16efc07a7
Vova <vkovalov23@gmail.com>
2021-04-09 22:31:18 +0200

    add DoubleClickListener.kt add change_color_logic of like

--- app/src/main/java/com/example/myapplication/ui/home/recycler/PostedImageViewHolder.kt
+++ app/src/main/java/com/example/myapplication/ui/home/recycler/PostedImageViewHolder.kt
@@ -1,10 +1,12 @@
 package com.example.myapplication.ui.home.recycler
 
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.myapplication.R
 import com.example.myapplication.ui.home.model.PostedItem
+import com.example.myapplication.utils.DoubleClickListener
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.posted_item.view.*
 
@@ -18,7 +20,23 @@ class PostedImageViewHolder(
         const val LAYOUT_ID = R.layout.posted_item
     }
 
+    var isLiked: Boolean = false
+
+    fun setLike(like: Boolean) {
+        if (like) {
+            containerView.ivIsLiked.setImageResource(R.drawable.ic_red_like)
+            containerView.ivBottomIsLiked.setImageResource(R.drawable.ic_red_like)
+        } else {
+            containerView.ivIsLiked.setImageResource(R.drawable.ic_grey_like)
+            containerView.ivBottomIsLiked.setImageResource(R.drawable.ic_grey_like)
+        }
+        isLiked = !isLiked
+    }
+
     fun bind(model: PostedItem) {
+
+        isLiked = model.isLiked
+
         containerView.tvTopProfileName.text = model.profile.profileName
         containerView.tvTopProfileName.setOnClickListener {
             action?.invoke(model.profile.profileLink)
@@ -27,6 +45,7 @@ class PostedImageViewHolder(
         Glide.with(containerView.context)
             .load(model.profile.profilePhoto)
             .into(containerView.civProfilePhoto)
+
         containerView.civProfilePhoto.setOnClickListener {
             action?.invoke(model.profile.profileLink)
         }
@@ -34,6 +53,15 @@ class PostedImageViewHolder(
         Glide.with(containerView.context)
             .load(model.imageUri)
             .into(containerView.ivPostPhoto)
+
+        containerView.ivPostPhoto.setOnClickListener(object : DoubleClickListener() {
+            override fun onDoubleClick(v: View) {
+                setLike(isLiked)
+//                isLiked = !isLiked
+                Toast.makeText(containerView.context, "rabotaet", Toast.LENGTH_SHORT).show()
+            }
+        })
+
 
 //        containerView.ivIsLiked.
 //        containerView.etAddComment.setOnClickListener {
@@ -44,7 +72,10 @@ class PostedImageViewHolder(
 //            action?.
 //        }
 
-//        containerView.ivBottomIsLiked
+        containerView.ivBottomIsLiked.setOnClickListener {
+            setLike(isLiked)
+//            isLiked = !isLiked
+        }
 
         containerView.tvLikeCounter.text = model.likesCount.toString()
 
